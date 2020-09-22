7f2c389f890cb1a82d58a7caae9fd76d47a99f66
Luke Brazil <Luke.brazil13@icloud.com>
2020-09-22 15:32:21 -0500

    prints out tasks not completed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,21 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const taskModel = require('../models/tasks');
-const taskData = taskModel.getTasks(); 
+const taskModel = require("../models/tasks");
 
-
-router.get('/to-do', (req,res) => {
-
-    console.log(taskData);
-    res.render('template', {
-        locals: {
-            title: "Tasks Page",
-            taskData: taskData
-        },
-        partials: {
-            partial: "partial-tasks"
-        }
-    })
-})
+router.get("/to-do", async (req, res) => {
+  const taskData = await taskModel.getTasks();
+  console.log(taskData);
+  res.render("template", {
+    locals: {
+      title: "Tasks Page",
+      taskData: taskData,
+    },
+    partials: {
+      partial: "partial-tasks",
+    },
+  });
+});
 
 module.exports = router;
